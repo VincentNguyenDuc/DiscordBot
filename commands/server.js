ce55aba5b5eb2bbcf7193cbf28f09c4ee256722e
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-01 00:44:53 +0700

    Add avatar commands

--- commands/server.js
+++ commands/server.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('server')
         .setDescription('Display info about this server.'),
     async execute(interaction) {
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+        return interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
     },
 };
