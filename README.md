4fcc4f4e4ca181aa2905c67b086ba571d91b7be1
Vincent <thanhvinhnguyenduc@gmail.com>
2023-01-31 17:31:44 +0700

    Update README

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 ## General Information
 
-For this project, I built a personal Discord Bot using Node.js and Discord.js
+For this project, I built a personal Discord Bot using Node.js and Discord.js. The source code is available at (<https://github.com/VincentNguyenDuc/DiscordBot>)
 
 ## Technologies
 
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 
 // Require
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
+
+const fs = require('node:fs');
+const path = require('node:path');
 
 // New client instance
 const client = new Client({intents : [GatewayIntentBits.Guilds]});
 
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+client.commands = new Collection();
 
-client.login(token);
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.nam, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    console.log(interaction);
+});
